38de2736958a03dd350559eb21a6f93235672a3b
Melissa Lopez <melilo89@gmail.com>
2023-07-31 18:31:37 -0300

    detalles de la busqueda

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,31 +1,31 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { filterName } from "../../Redux/actions";
+import { filterName, reset } from "../../Redux/actions";
 
 function SearchBar() {
   const dispatch = useDispatch();
   const { recipes } = useSelector((state) => state);
-console.log(recipes)
+
   const [searchValue, setSearchValue] = useState(""); // Estado local para mantener el valor del input
 
   const handleSearch = (event) => {
     const { value } = event.target;
-    console.log(value)
     setSearchValue(value); // Actualiza el estado local con el valor del input
 
-    const filterByName = recipes.filter((e) => {
-      return e.name.toLowerCase().includes(value.toLowerCase());
-    });
+    if (value.trim() === "") {
+      // Si el valor del input está vacío o contiene solo espacios en blanco
+      dispatch(reset()); // Despacha la acción reset para mostrar todas las recetas
+    } else {
+      const filterByName = recipes.filter((e) => {
+        return e.name.toLowerCase().includes(value.toLowerCase());
+      });
 
-    console.log(filterByName, 'filterbyname')
-    console.log(recipes, 'recipes')
-
-    // Enviamos el array filtrado para modificar el estado de recetas en Redux
-    dispatch(filterName(filterByName));
+      // Enviamos el array filtrado para modificar el estado de recetas en Redux
+      dispatch(filterName(filterByName));
+    }
   };
 
   return (
-    console.log('esto es searchvalue', searchValue),
     <div>
       <label htmlFor="">BUSCAR RECETAS POR NOMBRE</label>
       <input type="text" name="search" value={searchValue} onChange={handleSearch} />
